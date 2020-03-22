390cb83b7524e7c0ebf5001fc961a492f6140400
John Kitchin <johnrkitchin@gmail.com>
2020-03-22 13:45:08 -0400

    add gpu setup function

--- python/dftbook/setup.py
+++ python/dftbook/setup.py
@@ -1,13 +1,8 @@
 import subprocess
+import time
 
-def setup_colab():
-    '''Setup a colab environment.
-    Installs quantum-espresso, ase, the QE python wrapper,
-    '''
-    print('Please be patient. This takes about 30 seconds.')
-    print('Installing quantum espresso')
-    subprocess.run(['apt-get', 'install', 'quantum-espresso'])
 
+def setup_ase_espresso():
     print('Installing ASE')
     subprocess.run(['pip', 'install', 'ase'])
 
@@ -36,23 +31,68 @@ def setup_colab():
     print('Almost there, setting environment pseudopotential path')
     os.environ['ESP_PSP_PATH'] = '/content/gbrv_pseudopotentials/'
 
-    print('Setup is complete. Please visit https://github.com/jkitchin/dft-book-espresso to find the tutorials.')
 
-    # import webbrowser
-    #  webbrowser.open('https://drive.google.com/drive/folders/1fVOol26JssnCRXv_3EVgsbk3Z5jd60Nl')
-
-
-def setup_ubuntu():
-    '''Setup an Ubuntu environment.
-    I have nowhere to test this.
-    I assume you can just add "sudo" to the beginning of each command above.
-    Alternatively, we can make it a script in the package that you just run at the command line.
-    This probably needs some logic to not install every time it is run if not needed.
-    Maybe also
+def setup_colab():
+    '''Setup a colab environment.
+    Installs quantum-espresso, ase, the QE python wrapper,
     '''
-    raise NotImplementedError('This is not supported yet. Can you help?')
+    t0 = time.time()
+    print('Please be patient. This takes about 30 seconds.')
+
+    setup_ase_espresso()
+
+    print('Installing quantum espresso')
+    subprocess.run(['apt-get', 'install', 'quantum-espresso'])
+
+    print('Setup is complete. Please visit https://github.com/jkitchin/dft-book-espresso to find the tutorials.')
+    print(f'Installation took {time.time() - t0:1.1f} seconds')
+
+def setup_colab_gpu():
+    '''Setup a colab environment with a GPU.
+    You have to manually select a runtime with a GPU and then run this function.
+    '''
+    t0 = time.time()
+    print('Be patient. This usually takes about 30 seconds.')
+
+    subprocess.run(['pip', 'install', 'gputil'])
+    import GPUtil
+    GPUs = GPUtil.getGPUs()
+    if len(GPUs) == 0:
+        raise Exception('No GPU found. Did you select a Runtime with a GPU?')
+    print(f'Found {GPUs}')
+
+    setup_ase_espresso()
+
+    print("Installing MKL")
+    subprocess.run(["sh", "-c", "'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'"])
+    subprocess.run(["wget", "https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB"])
+    subprocess.run(["apt-key", "add", "GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB"])
+    subprocess.run(['sh', '-c', "'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'"])
+    subprocess.run(['apt', 'update'])
+    subprocess.run(['apt', 'install', 'intel-mkl-2020.0-088'])
+
+    print('Getting the GPU pw.x executable')
+    subprocess.run(['pip', 'install', 'gdown'])
+    url = 'https://drive.google.com/uc?id=1aWPj1yTXENx2tES_CVQxYFqhXQwqoHvI'
+    import gdown
+    gdown.download(url, '/usr/local/bin/pw.x', quiet=True)
+    subprocess.run(['chmod', '+x', '/usr/local/bin/pw.x'])
+
+    print('Done installing GPU version of Quantum Espresso')
+    print('Setup is complete. Please visit https://github.com/jkitchin/dft-book-espresso to find the tutorials.')
+    print(f'Installation took {time.time() - t0:1.1f} seconds')
+
+# def setup_ubuntu():
+#     '''Setup an Ubuntu environment.
+#     I have nowhere to test this.
+#     I assume you can just add "sudo" to the beginning of each command above.
+#     Alternatively, we can make it a script in the package that you just run at the command line.
+#     This probably needs some logic to not install every time it is run if not needed.
+#     Maybe also
+#     '''
+#     raise NotImplementedError('This is not supported yet. Can you help?')
 
 
-def setup_nersc():
-    '''Good idea?'''
-    raise NotImplementedError('This is not supported yet. Can you help?')
+# def setup_nersc():
+#     '''Good idea?'''
+#     raise NotImplementedError('This is not supported yet. Can you help?')
