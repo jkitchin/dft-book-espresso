57f348cda52f8343cf4a121edf7aba7d0f80b04c
John Kitchin <johnrkitchin@gmail.com>
2020-03-23 06:36:57 -0400

    add small output change for cpu

--- python/dftbook/setup.py
+++ python/dftbook/setup.py
@@ -30,6 +30,7 @@ def setup_ase_espresso():
     #Tell ase-espresso where to get the pseudopotentials
     print('Almost there, setting environment pseudopotential path')
     os.environ['ESP_PSP_PATH'] = '/content/gbrv_pseudopotentials/'
+    print('Done setting up ase, ase-espresso and the pseudopotentials')
 
 
 def setup_colab():
@@ -42,6 +43,7 @@ def setup_colab():
     setup_ase_espresso()
 
     if os.environ['COLAB_GPU']=='1': #we have a gpu
+        print('Found a GPU, installing the GPU Quantum Espresso version')
         print("Installing MKL")
         subprocess.run(["sh", "-c", "'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'"])
         subprocess.run(["wget", "https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB"])
@@ -59,9 +61,9 @@ def setup_colab():
         print('Done installing GPU version of Quantum Espresso')
 
     else: #cpu-only colab
-        print('Installing quantum espresso')
+        print('Installing quantum espresso for CPU')
         subprocess.run(['apt-get', 'install', 'quantum-espresso'])
-        
+
     print('Setup is complete. Please visit https://github.com/jkitchin/dft-book-espresso to find the tutorials.')
     print(f'Installation took {time.time() - t0:1.1f} seconds')
 
